34335641d0dd9b71099264cf823e2ebbe73eb163
stedsou <stephen_michael.d_souza@mercedes-benz.com>
2025-05-17 19:03:17 +0200

    Added proper naming

--- src/es/consts.py
+++ src/es/consts.py
@@ -3,10 +3,10 @@
 # The "8" in "compatible-with=8" indicates that the client is compatible with version 8 of Elasticsearch.
 # This should only be changed if the server version changes and the client needs to be compatible with a different version.
 
-x = "application/vnd.elasticsearch+json; compatible-with=8"
+ES_MIME_TYPE = "application/vnd.elasticsearch+json; compatible-with=8"
 HEADER = {
-    "Accept": x,
-    "Content-Type": x,
+    "Accept": ES_MIME_TYPE,
+    "Content-Type": ES_MIME_TYPE,
 }
 
 INDEX = "imago"
